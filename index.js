18d2ce71c8541ec169cb1184f762fceb58a5a325
Dennis Prazak <prazak.dennis@gmail.com>
2019-02-16 19:53:24 +0100

    Add spam command

--- index.js
+++ index.js
@@ -32,6 +32,45 @@ async function recognizePokemon () {
   return name
 }
 
+async function handleRetrogradeRecognition (id, channel) {
+  console.log('.mm command issued')
+  let fetched = null
+  try {
+    fetched = await channel.fetchMessage(id)
+  } catch (e) {
+    channel.send('Could not fetch message ' + id).then(m => setTimeout(() => m.delete(), 4000))
+    return
+  }
+  try {
+    let url = fetched.embeds[0].image.url
+    request(url).pipe(fs.createWriteStream('temp.jpg')).on('close', async () => {
+      channel.send(`p!catch ${await recognizePokemon()}`)
+    })
+  } catch (e) {
+    channel.send('It seems that message is not a wild Pokémon.').then(m => setTimeout(() => m.delete(), 4000))
+  }
+}
+
+var spamInterval = null
+
+function startSpam (channel) {
+  let words = [ 'hello', 'hi', 'greetings', 'good day to you', 'welcome, traveler!' ]
+  function randomWord () {
+    let index = Math.floor(Math.random() * words.length)
+    return words[index]
+  }
+  spamInterval = setInterval(() => {
+    channel.send(randomWord())
+  }, 4000)
+  console.log('spam started')
+}
+
+function stopSpam () {
+  clearInterval(spamInterval)
+  spamInterval = null
+  console.log('spam stopped')
+}
+
 client.on('message', async msg => {
   if (msg.channel.id === channelId && msg.author.id === pokecordId) {
     console.log('Message form Pokécord', msg.embeds.length)
@@ -47,24 +86,16 @@ client.on('message', async msg => {
 
   if (msg.channel.id === channelId && msg.author.id === client.user.id) {
     let r = /^\.mm\s+(\d+)/.exec(msg.content)
-    if (r === null) return
-    console.log('.mm command issued')
-    await msg.delete()
-    let msgId = r[1]
-    let fetched = null
-    try {
-      fetched = await msg.channel.fetchMessage(msgId)
-    } catch (e) {
-      msg.channel.send('Could not fetch message ' + msgId).then(m => setTimeout(() => m.delete(), 4000))
+    if (r !== null) {
+      msg.delete()
+      await handleRetrogradeRecognition(r[1], msg.channel)
       return
     }
-    try {
-      let url = fetched.embeds[0].image.url
-      request(url).pipe(fs.createWriteStream('temp.jpg')).on('close', async () => {
-        msg.channel.send(`p!catch ${await recognizePokemon()}`)
-      })
-    } catch (e) {
-      msg.channel.send('It seems that message is not a wild Pokémon.').then(m => setTimeout(() => m.delete(), 4000))
+    r = /^.spam/.test(msg.content)
+    if (r) {
+      msg.delete()
+      if (spamInterval === null) startSpam(msg.channel)
+      else stopSpam()
     }
   }
 })
